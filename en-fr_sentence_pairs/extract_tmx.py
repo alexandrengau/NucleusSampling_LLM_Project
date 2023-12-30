ed1d8cca514b9632a8d5f56b5a635165db8c63a8
Alexandre Ngau <alexandrengau@hotmail.fr>
2023-12-30 23:43:48 +0100

    adding a dataset of en-fr sentence pairs, and modifying nmt_nucleus_sampling.py accordinlgy

---
+++ en-fr_sentence_pairs/extract_tmx.py
@@ -0,0 +1,42 @@
+import json
+import xml.etree.ElementTree as ET
+
+
+# Define a function to extract sentence pairs from a TMX file
+def extract_sentence_pairs(tmx_file_path):
+    # Parse the TMX file using ElementTree
+    tree = ET.parse(tmx_file_path)
+    root = tree.getroot()
+
+    # Define the XML namespace (used for handling XML namespace in the TMX file)
+    ns = {'xml': 'http://www.w3.org/XML/1998/namespace'}
+
+    # Initialize an empty list to store extracted sentence pairs
+    sentence_pairs = []
+
+    # Iterate through translation units ('tu' elements) in the TMX file
+    for tu in root.findall(".//tu", namespaces=ns):
+        # Extract source and target segments for each translation unit
+        source_segment = tu.find(".//tuv[@xml:lang='en']/seg", namespaces=ns).text
+        target_segment = tu.find(".//tuv[@xml:lang='fr']/seg", namespaces=ns).text
+
+        # Append the extracted pair to the list
+        sentence_pairs.append({"source": source_segment, "target": target_segment})
+
+    # Return the list of extracted sentence pairs
+    return sentence_pairs
+
+
+# Replace 'en-fr.tmx' with the path to your TMX file
+tmx_file_path = './en-fr_sentence_pairs/en-fr.tmx'
+# Call the function to extract sentence pairs
+sentence_pairs = extract_sentence_pairs(tmx_file_path)
+
+# Save the extracted sentence pairs to a JSON file
+json_file_path = './en-fr_sentence_pairs/en-fr_sentence_pairs.json'
+with open(json_file_path, 'w', encoding='utf-8') as json_file:
+    # Serialize the list of sentence pairs to JSON with indentation
+    json.dump(sentence_pairs, json_file, ensure_ascii=False, indent=2)
+
+# Print a message indicating the successful save of the JSON file
+print(f"Sentence pairs saved to {json_file_path}")
--- nmt_nucleus_sampling.py
+++ nmt_nucleus_sampling.py
@@ -6,20 +6,21 @@ import random
 import numpy as np
 
 
-def load_random_paragraph(json_path):
-    # Open the JSON file containing paragraphs
+def load_random_sentence_pairs(json_path):
+    # Open the JSON file containing sentence pairs
     with open(json_path, 'r', encoding='utf-8') as json_file:
         # Load JSON data
         data = json.load(json_file)
 
-        # Extract paragraphs from the loaded data
-        paragraphs = data['paragraphs']
+        # Choose a random sentence pair from the list
+        random_sentence_pair = random.choice(data)
 
-        # Choose a random paragraph from the list
-        random_paragraph = random.choice(paragraphs)
+        # Extract source and target sentences
+        source_sentence = random_sentence_pair["source"]
+        target_sentence = random_sentence_pair["target"]
 
-        # Return the randomly selected paragraph
-        return random_paragraph
+        # Return the randomly selected sentence pair
+        return source_sentence, target_sentence
 
 
 def translate(model, tokenizer, input_text, max_length=512, top_p=0.5):
@@ -53,16 +54,16 @@ def translate(model, tokenizer, input_text, max_length=512, top_p=0.5):
     return decoded_translation_top_p, decoded_translation_default
 
 
-def compute_perplexity(model, tokenizer, input_text, generated_text):
-    # Encode the input and generated text using the tokenizer
-    input_ids = tokenizer.encode(input_text, return_tensors='pt')
+def compute_perplexity(model, tokenizer, reference_text, generated_text):
+    # Encode the reference (ground truth) and generated text using the tokenizer
+    reference_ids = tokenizer.encode(reference_text, return_tensors='pt')
     generated_ids = tokenizer.encode(generated_text, return_tensors='pt')
 
     # Disable gradient computation during inference
     with torch.no_grad():
         # Generate logits using the language model
         logits = model(
-            input_ids=input_ids,
+            input_ids=reference_ids,
             decoder_input_ids=generated_ids
         ).logits
 
@@ -83,46 +84,36 @@ model_name = "Helsinki-NLP/opus-mt-en-fr"
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-# Load a random paragraph from the JSON file
-json_path = './book_metadata/parsed_book.json'
-input_text = load_random_paragraph(json_path)
+# Load a random sentence pair from the JSON file
+json_path = './en-fr_sentence_pairs/en-fr_sentence_pairs.json'
+source_text, target_text = load_random_sentence_pairs(json_path)
 
-# Generate translations using both top-p sampling and default generation (beam search) without progress bars
-translation_top_p, translation_default = translate(model, tokenizer, input_text)
+# Generate translations using both top-p sampling and default generation (beam search)
+translation_top_p, translation_default = translate(model, tokenizer, source_text)
 
+# Compute reference perplexity for the model on the given dataset
+reference_perplexity = compute_perplexity(model, tokenizer, target_text, target_text)
 # Compute perplexities for each generated sequence
-perplexity_default = compute_perplexity(model, tokenizer, input_text, translation_default)
-perplexity_top_p = compute_perplexity(model, tokenizer, input_text, translation_top_p)
+perplexity_default = compute_perplexity(model, tokenizer, target_text, translation_default)
+perplexity_top_p = compute_perplexity(model, tokenizer, target_text, translation_top_p)
 
-"""
-When you compute compute_perplexity(model, tokenizer, input_text, generated_text) for a human-written input_text 
-and its NMT-generated translation (generated_text), you are essentially assessing how well the language model (NMT 
-model) predicts the generated translation given the original text. Perplexity measures how well the model's predicted 
-distribution aligns with the actual tokens in the generated sequence.
-"""
+# Calculate absolute differences
+abs_diff_default = abs(reference_perplexity - perplexity_default)
+abs_diff_top_p = abs(reference_perplexity - perplexity_top_p)
+
+# Print sentences
+print("Source Text:\n", source_text)
+print("Target Text:\n", target_text, "\n")
 
 # Print translations
-print("Input Text:\n", input_text, "\n")
-print("Translation (Default Generation - Beam Search):\n", translation_default, "\n")
+print("Translation (Default Generation - Beam Search):\n", translation_default)
 print("Translation (Top-p Sampling):\n", translation_top_p, "\n")
 
-# Print and compare perplexities
+# Print perplexities
+print(f"Reference Perplexity for the Model: {reference_perplexity:.2f}")
 print(f"Perplexity for Translation (Default Generation): {perplexity_default:.2f}")
 print(f"Perplexity for Translation (Top-p Sampling): {perplexity_top_p:.2f}")
 
-
-"""
---- GREAT EXAMPLE BELOW ---
-
-Input Text:
- Mrs. Bradshaw's 'Well! Well!' seemed to sum up the general feeling; Mr. Kemp, shaking his head, eyed him with gentle reproach. 
-
-Translation (Default Generation - Beam Search (b=4)):
- Mme Bradshaw 'Eh bien! Eh bien!' semblait résumer le sentiment général; M. Kemp, secouant la tête, le regardait avec un doux reproche. 
-
-Translation (Top-p Sampling (p=0.5)):
- L'expression «Eh bien! Eh bien!» de Mme Bradshaw semblait résumer la sensation générale ; M. Kemp, agitant la tête, le regardait avec une douce opprobre. 
-
-Perplexity for Translation (Default Generation): 1197.26
-Perplexity for Translation (Top-p Sampling): 2373.95
-"""
+# Print absolute differences
+print(f"\nAbsolute Difference (Reference vs. Default Generation): {abs_diff_default:.2f}")
+print(f"Absolute Difference (Reference vs. Top-p Sampling): {abs_diff_top_p:.2f}")
